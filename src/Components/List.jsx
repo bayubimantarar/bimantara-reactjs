69133d1f92e6f726b20c4735d58ef85cf3623f36
Bayu Bimantara <bayubimantarar@gmail.com>
2018-07-24 19:07:19 +0700

    Initial Commit

---
+++ src/Components/List.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        <h1>List</h1>
+        <ul>
+          <li>Read book</li>
+          <li>Coding</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default List;
